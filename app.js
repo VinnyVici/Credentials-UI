bee3c9793b81913d8841f01cef2978610b6870fb
VinnyVici <VinnyVici@users.noreply.github.com>
2025-08-28 12:06:11 -0700

    Build environment

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProxyCredentialsUI from './ProxyCredentialsUI';
+
+function App() {
+  return (
+    <div className="App">
+      <ProxyCredentialsUI />
+    </div>
+  );
+}
+
+export default App;
